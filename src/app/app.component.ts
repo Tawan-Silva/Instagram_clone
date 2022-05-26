e07c9e3951b9ad7eb112dcd29f10768b85061355
Tawan Silva <tawan.tls43@gmail.com>
2022-05-26 09:12:01 -0300

    Definindo comunicacao firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'app3';
+
+
+  ngOnInit(): void {
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyAF4QQW47U5mByw4y19u35V4mqqssjVvFg",
+      authDomain: "jta-instagram-clone-eda63.firebaseapp.com",
+      projectId: "jta-instagram-clone-eda63",
+      storageBucket: "jta-instagram-clone-eda63.appspot.com",
+      messagingSenderId: "866357845089",
+      appId: "1:866357845089:web:15094826216050f7b41e20",
+      databaseURL: "https://jta-instagram-clone-eda63-default-rtdb.firebaseio.com/"
+    };
+
+    firebase.initializeApp(firebaseConfig)
+  }
 }
